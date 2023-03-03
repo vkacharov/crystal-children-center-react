a5934d2b275a8738afb1a3030009838580d10786
vkacharov <velizar.kacharov@gmail.com>
2023-03-03 22:33:12 +0200

    Replace the Visit link with a Donate link

--- src/ui-components/ResponsiveNavBar.d.ts
+++ src/ui-components/ResponsiveNavBar.d.ts
@@ -6,7 +6,7 @@
 
 import * as React from "react";
 import { EscapeHatchProps } from "@aws-amplify/ui-react/internal";
-import { DividerProps, FlexProps, ImageProps, TextProps } from "@aws-amplify/ui-react";
+import { DividerProps, FlexProps, IconProps, ImageProps, TextProps } from "@aws-amplify/ui-react";
 import { SyntheticEvent } from "react";
 export declare type PrimitiveOverrideProps<T> = Partial<T> & React.DOMAttributes<HTMLDivElement>;
 export declare type ResponsiveNavBarOverridesProps = {
@@ -16,13 +16,15 @@ export declare type ResponsiveNavBarOverridesProps = {
     "Logo_text_white 1"?: PrimitiveOverrideProps<ImageProps>;
     Divider37852694?: PrimitiveOverrideProps<DividerProps>;
     HomeFrame?: PrimitiveOverrideProps<FlexProps>;
+    Vector38222704?: PrimitiveOverrideProps<IconProps>;
     Home?: PrimitiveOverrideProps<TextProps>;
-    "Frame 321"?: PrimitiveOverrideProps<FlexProps>;
-    Divider37852696?: PrimitiveOverrideProps<DividerProps>;
-    VisitFrame?: PrimitiveOverrideProps<FlexProps>;
-    visit?: PrimitiveOverrideProps<TextProps>;
+    External?: PrimitiveOverrideProps<FlexProps>;
+    DonateFrame?: PrimitiveOverrideProps<FlexProps>;
+    Vector38312708?: PrimitiveOverrideProps<IconProps>;
+    donate?: PrimitiveOverrideProps<TextProps>;
     Divider37782775?: PrimitiveOverrideProps<DividerProps>;
     SignoutFrame?: PrimitiveOverrideProps<FlexProps>;
+    Vector38222709?: PrimitiveOverrideProps<IconProps>;
     signout?: PrimitiveOverrideProps<TextProps>;
 } & EscapeHatchProps;
 export declare type ResponsiveNavBarProps = React.PropsWithChildren<Partial<FlexProps> & {
--- src/ui-components/ResponsiveNavBar.jsx
+++ src/ui-components/ResponsiveNavBar.jsx
@@ -15,6 +15,7 @@ import {
 import {
   Divider,
   Flex,
+  Icon,
   Image,
   Text,
   useBreakpointValue,
@@ -27,16 +28,18 @@ export default function ResponsiveNavBar(props) {
         "Logo_text_white 1": {},
         Logo: {},
         Divider37852694: {},
+        Vector38222704: {},
         Home: {},
         HomeFrame: {},
         Navigation: {},
-        Divider37852696: {},
-        visit: {},
-        VisitFrame: {},
+        Vector38312708: {},
+        donate: {},
+        DonateFrame: {},
         Divider37782775: {},
+        Vector38222709: {},
         signout: {},
         SignoutFrame: {},
-        "Frame 321": {},
+        External: {},
         ResponsiveNavBar: {},
       },
       variantValues: { breakpoint: "medium" },
@@ -46,16 +49,22 @@ export default function ResponsiveNavBar(props) {
         "Logo_text_white 1": { width: "45px", height: "45px", shrink: "0" },
         Logo: { width: "45px", height: "45px" },
         Divider37852694: { height: "24px" },
-        Home: {},
+        Vector38222704: {},
+        Home: { display: "none" },
         HomeFrame: {},
-        Navigation: { gap: "16px" },
-        Divider37852696: { height: "24px" },
-        visit: { display: "block", children: "v" },
-        VisitFrame: {},
+        Navigation: { gap: "14px", justifyContent: "center" },
+        Vector38312708: {},
+        donate: { display: "none", children: "v" },
+        DonateFrame: {
+          gap: "10px",
+          justifyContent: "flex-start",
+          alignItems: "flex-start",
+        },
         Divider37782775: {},
-        signout: {},
-        SignoutFrame: {},
-        "Frame 321": {},
+        Vector38222709: {},
+        signout: { display: "none" },
+        SignoutFrame: { gap: "10px" },
+        External: { gap: "14px", justifyContent: "center" },
         ResponsiveNavBar: { width: "350px", padding: "6px 18px 6px 18px" },
       },
       variantValues: { breakpoint: "small" },
@@ -65,19 +74,18 @@ export default function ResponsiveNavBar(props) {
         "Logo_text_white 1": {},
         Logo: {},
         Divider37852694: { height: "24px" },
+        Vector38222704: {},
         Home: {},
-        HomeFrame: {},
+        HomeFrame: { gap: "8px" },
         Navigation: { gap: "32px", justifyContent: "center" },
-        Divider37852696: {},
-        visit: {
-          display: "block",
-          children: "Visit chrystalchildrenscenter.com",
-        },
-        VisitFrame: { display: "flex" },
+        Vector38312708: {},
+        donate: {},
+        DonateFrame: { gap: "8px" },
         Divider37782775: {},
+        Vector38222709: {},
         signout: {},
-        SignoutFrame: {},
-        "Frame 321": {},
+        SignoutFrame: { gap: "8px" },
+        External: { gap: "32px" },
         ResponsiveNavBar: { width: "1260px" },
       },
       variantValues: { breakpoint: "large" },
@@ -87,16 +95,30 @@ export default function ResponsiveNavBar(props) {
         "Logo_text_white 1": { width: "45px", height: "45px", shrink: "0" },
         Logo: { width: "45px", height: "45px" },
         Divider37852694: { height: "24px" },
-        Home: { fontSize: "18px", letterSpacing: "0.03px" },
-        HomeFrame: {},
-        Navigation: { gap: "10px" },
-        Divider37852696: { height: "24px" },
-        visit: { display: "block", children: "v" },
-        VisitFrame: {},
+        Vector38222704: {},
+        Home: { fontSize: "18px", display: "none", letterSpacing: "0.03px" },
+        HomeFrame: { gap: "4px" },
+        Navigation: { gap: "14px" },
+        Vector38312708: {},
+        donate: { display: "none", children: "v" },
+        DonateFrame: {
+          gap: "10px",
+          justifyContent: "flex-start",
+          alignItems: "flex-start",
+        },
         Divider37782775: {},
-        signout: { fontSize: "18px", lineHeight: "21.784090042114258px" },
-        SignoutFrame: {},
-        "Frame 321": {},
+        Vector38222709: {},
+        signout: {
+          fontSize: "18px",
+          lineHeight: "21.784090042114258px",
+          display: "none",
+        },
+        SignoutFrame: {
+          gap: "10px",
+          justifyContent: "flex-start",
+          alignItems: "flex-start",
+        },
+        External: { gap: "14px" },
         ResponsiveNavBar: {
           gap: "18px",
           width: "300px",
@@ -122,10 +144,10 @@ export default function ResponsiveNavBar(props) {
   );
   const logoOnClick = useNavigateAction({ type: "url", url: "/" });
   const homeFrameOnClick = useNavigateAction({ type: "url", url: "/" });
-  const visitFrameOnClick = useNavigateAction({
+  const donateFrameOnClick = useNavigateAction({
     target: "_blank",
     type: "url",
-    url: "https://www.chrystalchildrenscenter.com/",
+    url: "https://www.chrystalchildrenscenter.com/donate/",
   });
   return (
     <Flex
@@ -145,7 +167,7 @@ export default function ResponsiveNavBar(props) {
       {...rest}
     >
       <Flex
-        gap="40px"
+        gap="18px"
         direction="row"
         width="unset"
         height="unset"
@@ -201,12 +223,12 @@ export default function ResponsiveNavBar(props) {
           {...getOverrideProps(overrides, "Divider37852694")}
         ></Divider>
         <Flex
-          gap="10px"
+          gap="6px"
           direction="row"
           width="unset"
           height="unset"
-          justifyContent="flex-start"
-          alignItems="flex-start"
+          justifyContent="center"
+          alignItems="center"
           shrink="0"
           position="relative"
           padding="0px 0px 0px 0px"
@@ -217,6 +239,25 @@ export default function ResponsiveNavBar(props) {
           }}
           {...getOverrideProps(overrides, "HomeFrame")}
         >
+          <Icon
+            width="23px"
+            height="17.5px"
+            viewBox={{ minX: 0, minY: 0, width: 23, height: 17.5 }}
+            paths={[
+              {
+                d: "M3 17C3 17.2761 3.22386 17.5 3.5 17.5L19.5 17.5C19.7761 17.5 20 17.2761 20 17L20 9L22.189 9C22.4675 9 22.5957 8.65348 22.3843 8.47222L19.5 6L19.5 1.2C19.5 1.08954 19.4105 1 19.3 1L16.2 1C16.0895 1 16 1.08954 16 1.2L16 3.5L12.4238 0.718485C11.8808 0.296159 11.1201 0.297302 10.5784 0.721257L0.68521 8.46375C0.460619 8.63952 0.584909 9 0.870103 9L3 9L3 17ZM14.5 17.5L8.5 17.5L8.5 11.5C8.5 10.9477 8.94772 10.5 9.5 10.5L13.5 10.5C14.0523 10.5 14.5 10.9477 14.5 11.5L14.5 17.5Z",
+                fill: "rgba(13,26,38,1)",
+                fillRule: "nonzero",
+              },
+            ]}
+            display="block"
+            gap="unset"
+            alignItems="unset"
+            justifyContent="unset"
+            shrink="0"
+            position="relative"
+            {...getOverrideProps(overrides, "Vector38222704")}
+          ></Icon>
           <Text
             fontFamily="Inter"
             fontSize="20px"
@@ -242,7 +283,7 @@ export default function ResponsiveNavBar(props) {
         </Flex>
       </Flex>
       <Flex
-        gap="32px"
+        gap="18px"
         direction="row"
         width="unset"
         height="unset"
@@ -252,34 +293,44 @@ export default function ResponsiveNavBar(props) {
         position="relative"
         padding="0px 0px 0px 0px"
         display="flex"
-        {...getOverrideProps(overrides, "Frame 321")}
+        {...getOverrideProps(overrides, "External")}
       >
-        <Divider
-          height="unset"
-          display="none"
-          shrink="0"
-          alignSelf="stretch"
-          size="small"
-          orientation="vertical"
-          {...getOverrideProps(overrides, "Divider37852696")}
-        ></Divider>
         <Flex
-          gap="10px"
+          gap="6px"
           direction="row"
           width="unset"
           height="unset"
-          justifyContent="flex-start"
-          alignItems="flex-start"
+          justifyContent="center"
+          alignItems="center"
           shrink="0"
           position="relative"
           padding="0px 0px 0px 0px"
-          display="none"
+          display="flex"
           className="action-link"
           onClick={() => {
-            visitFrameOnClick();
+            donateFrameOnClick();
           }}
-          {...getOverrideProps(overrides, "VisitFrame")}
+          {...getOverrideProps(overrides, "DonateFrame")}
         >
+          <Icon
+            width="20px"
+            height="18.35px"
+            viewBox={{ minX: 0, minY: 0, width: 20, height: 18.35009765625 }}
+            paths={[
+              {
+                d: "M10 18.35L8.55 17.03C3.4 12.36 0 9.28 0 5.5C0 2.42 2.42 0 5.5 0C7.24 0 8.91 0.81 10 2.09C11.09 0.81 12.76 0 14.5 0C17.58 0 20 2.42 20 5.5C20 9.28 16.6 12.36 11.45 17.04L10 18.35Z",
+                fill: "rgba(13,26,38,1)",
+                fillRule: "nonzero",
+              },
+            ]}
+            display="block"
+            gap="unset"
+            alignItems="unset"
+            justifyContent="unset"
+            shrink="0"
+            position="relative"
+            {...getOverrideProps(overrides, "Vector38312708")}
+          ></Icon>
           <Text
             fontFamily="Inter"
             fontSize="20px"
@@ -287,7 +338,7 @@ export default function ResponsiveNavBar(props) {
             color="rgba(0,0,0,1)"
             lineHeight="24.204544067382812px"
             textAlign="left"
-            display="none"
+            display="block"
             direction="column"
             justifyContent="unset"
             width="unset"
@@ -299,8 +350,8 @@ export default function ResponsiveNavBar(props) {
             padding="0px 0px 0px 0px"
             whiteSpace="pre-wrap"
             className=""
-            children="visit"
-            {...getOverrideProps(overrides, "visit")}
+            children="Donate"
+            {...getOverrideProps(overrides, "donate")}
           ></Text>
         </Flex>
         <Divider
@@ -312,12 +363,12 @@ export default function ResponsiveNavBar(props) {
           {...getOverrideProps(overrides, "Divider37782775")}
         ></Divider>
         <Flex
-          gap="10px"
+          gap="6px"
           direction="row"
           width="unset"
           height="unset"
-          justifyContent="flex-start"
-          alignItems="flex-start"
+          justifyContent="center"
+          alignItems="center"
           shrink="0"
           position="relative"
           padding="0px 0px 0px 0px"
@@ -326,6 +377,25 @@ export default function ResponsiveNavBar(props) {
           onClick={onSignOutClick}
           {...getOverrideProps(overrides, "SignoutFrame")}
         >
+          <Icon
+            width="16px"
+            height="16px"
+            viewBox={{ minX: 0, minY: 0, width: 16, height: 16 }}
+            paths={[
+              {
+                d: "M8 0L6.59 1.41L12.17 7L0 7L0 9L12.17 9L6.59 14.59L8 16L16 8L8 0Z",
+                fill: "rgba(13,26,38,1)",
+                fillRule: "nonzero",
+              },
+            ]}
+            display="block"
+            gap="unset"
+            alignItems="unset"
+            justifyContent="unset"
+            shrink="0"
+            position="relative"
+            {...getOverrideProps(overrides, "Vector38222709")}
+          ></Icon>
           <Text
             fontFamily="Inter"
             fontSize="20px"
